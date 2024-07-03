e108791c5e685878bd5ac3a7bb945b4c4a4c0b40
aniket singh <aniket.singh@iot83.com>
2024-07-03 08:39:01 +0530

    learn: how to add nested elements and siblind using React.createElement

---
+++ app.js
@@ -0,0 +1,27 @@
+const parent = React.createElement(
+  "div", //tag Name
+  { id: "parent" }, //attributes
+  [
+    React.createElement(
+      //children
+      "div",
+      { id: "childOne" },
+      [
+        React.createElement("h1", { id: "heading" }, "I am an h1 tag"),
+        React.createElement("h2", { id: "heading" }, "I am an h2 tag"),
+      ]
+    ),
+    React.createElement(
+      //children
+      "div",
+      { id: "childTwo" },
+      [
+        React.createElement("h1", { id: "heading" }, "I am an h1 tag"),
+        React.createElement("h2", { id: "heading" }, "I am an h2 tag"),
+      ]
+    ),
+  ] //children (siblings)
+); //{} takes attributes for an tag
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
