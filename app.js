b814d660a65ff29adb69bf212e10b2d3de91317c
Aniket Singh <aniketsingh90126@gmail.com>
2024-07-11 12:49:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,77 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const parent = React.createElement(
-  "div", //tag Name
-  { id: "parent" }, //attributes
-  [
-    React.createElement(
-      //children
-      "div",
-      { id: "childOne" },
-      [
-        React.createElement("h1", { id: "heading" }, "I am an h1 tag"),
-        React.createElement("h2", { id: "heading" }, "I am an h2 tag"),
-      ]
-    ),
-    React.createElement(
-      //children
-      "div",
-      { id: "childTwo" },
-      [
-        React.createElement("h1", { id: "heading" }, "I am an h1 tag"),
-        React.createElement("h2", { id: "heading" }, "I am an h2 tag"),
-      ]
-    ),
-  ] //children (siblings)
-); //{} takes attributes for an tag
-console.log(parent);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          height={"80px"}
+          width={"90px"}
+          alt="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&txt_keyword=All"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = ({ restaurantName, cuisine, star }) => {
+  return (
+    <div className="restaurant-card">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlMyzfmXp2bWMGCMLw2JC4uXpXR1qEGTCBvw&s"
+        alt="food"
+      />
+      <h3>{restaurantName}</h3>
+      <div className="card-footer">
+        <p>{cuisine}</p>
+        <p>{star}🌟</p>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restaurant-container">
+        <RestaurantCard
+          restaurantName={"Meghna Foods"}
+          cuisine="North Indian"
+          star={5}
+        />
+        <RestaurantCard
+          restaurantName={"KFC"}
+          cuisine="
+        Non-veg"
+          star={4}
+        />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<AppLayout />);
